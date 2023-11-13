3094277349f57acd399dde60d6c953f9cd1da121
Y-io <yiobincn@gmail.com>
2023-11-13 16:32:15 +0800

    feat: 数据序列化，响应脱敏

---
+++ src/common/interceptors/serialize.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { plainToInstance } from 'class-transformer';
+
+@Injectable()
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(
+    private dto: any,
+    private excludeExtraneousValues = true,
+  ) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        return plainToInstance(this.dto, data, {
+          // true --> 必须加 @Expose() 才会显示
+          // false --> 加 @Exclude() 才会被排除
+          excludeExtraneousValues: this.excludeExtraneousValues,
+        });
+      }),
+    );
+  }
+}
