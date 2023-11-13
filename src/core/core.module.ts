755121ec7f742e0ae520bc677f2281d76615bd07
Y-io <yiobincn@gmail.com>
2023-11-13 15:26:35 +0800

    基础架构

---
+++ src/core/core.module.ts
@@ -0,0 +1,87 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MurLockModule } from 'murlock';
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import { MurLockModuleOptions } from 'murlock/dist/interfaces/murlock-options.interface';
+
+/**
+ * 创建一个每日轮转的传输对象。
+ *
+ * @param {string} level - 日志级别。
+ * @param {string} filename - 日志文件的基本文件名。
+ * @return {DailyRotateFile} - DailyRotateFile 对象。
+ */
+function createDailyRotateTransport(level: string, filename: string) {
+  return new DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
+  });
+}
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MurLockModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const murLockModuleOptions = {
+          redisOptions: {
+            host: configService.get('REDIS_HOST') as number,
+            port: configService.get('REDIS_PORT'),
+          },
+          wait: configService.get('REDIS_WAIT'),
+          maxAttempts: configService.get('REDIS_MAX_ATTEMPTS'),
+          logLevel: configService.get('REDIS_LOG_LEVEL'),
+        } as MurLockModuleOptions;
+
+        return murLockModuleOptions;
+      },
+    }),
+    WinstonModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProEnv = configService.get('ENVIRONMENT') === 'production';
+        const transportList = isProEnv
+          ? [
+              createDailyRotateTransport('error', 'error'),
+              createDailyRotateTransport('info', 'app'),
+            ]
+          : [];
+        return {
+          transports: [
+            new winston.transports.Console({
+              format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.ms(),
+                nestWinstonModuleUtilities.format.nestLike('MyApp', {
+                  colors: true,
+                  prettyPrint: true,
+                }),
+              ),
+            }),
+            ...transportList,
+          ],
+        };
+      },
+    }),
+    PrismaModule,
+  ],
+})
+export class CoreModule {}
---
+++ src/main.ts
@@ -0,0 +1,64 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import * as compression from 'compression';
+
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3000;
+  const globalPrefix = 'api';
+
+  // 使用 winston 日志库替代默认日志
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  app.setGlobalPrefix(globalPrefix);
+
+  // 版本号
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1'],
+  });
+
+  // 全局参数过滤
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // transform: true,
+      whitelist: true,
+    }),
+  );
+
+  // 启用 gzip 压缩
+  app.use(compression());
+
+  // 允许跨域
+  app.enableCors();
+
+  // 通过适当设置 HTTP 标头，Helmet 可以帮助保护您的应用程序免受一些众所周知的 Web 漏洞的影响。 一般来说，Helmet 只是一些较小的中间件功能的集合，用于设置与安全相关的 HTTP 标头
+  app.use(helmet());
+
+  // 限速
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  // 如果在服务器和以太网之间存在负载均衡或者反向代理，Express 可能需要配置为信任 proxy 设置的头文件，从而保证最终用户得到正确的 IP 地址
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // see https://expressjs.com/en/guide/behind-proxies.html
+  // app.set('trust proxy', 1);
+
+  await app.listen(port);
+
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+}
+bootstrap();
